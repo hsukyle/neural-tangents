34b03b2b36a39b48993e569f73c972274ac6f31e
Kyle Hsu <kylehkhsu@gmail.com>
2019-06-12 12:26:12 -0400

    adopt ntk parameterization

--- maml/network.py
+++ maml/network.py
@@ -2,8 +2,10 @@ from jax.experimental import stax
 from jax.experimental.stax import (AvgPool, BatchNorm, Conv, Dense, FanInSum,
                                    FanOut, Flatten, GeneralConv, Identity,
                                    MaxPool, Relu, Tanh, LogSoftmax)
+from neural_tangents import layers
 
-def denseActivationNormLayer(n_hidden_unit, activation, norm):
+
+def denseActivationNormLayer(n_hidden_unit, bias_coef, activation, norm):
     if activation == 'relu':
         activation = Relu
     elif activation == 'tanh':
@@ -14,20 +16,27 @@ def denseActivationNormLayer(n_hidden_unit, activation, norm):
         raise ValueError
 
     if norm is None:
-        return stax.serial(Dense(n_hidden_unit), activation)
+        return stax.serial(
+            layers.Dense(n_hidden_unit, b_gain=bias_coef),
+            activation
+        )
     elif norm == 'batch_norm':
-        return stax.serial(Dense(n_hidden_unit), activation, BatchNorm(axis=0))
+        return stax.serial(
+            layers.Dense(n_hidden_unit, b_gain=bias_coef),
+            activation,
+            BatchNorm(axis=0)
+        )
     else:
         raise ValueError
 
-def mlp(n_output, n_hidden_layer, n_hidden_unit, activation='relu', norm=None):
 
+def mlp(n_output, n_hidden_layer, n_hidden_unit, bias_coef, activation='relu', norm=None):
     return stax.serial(
-        *[denseActivationNormLayer(n_hidden_unit, activation, norm) for i in range(n_hidden_layer)],
-        Dense(n_output)
+        *[denseActivationNormLayer(n_hidden_unit, bias_coef, activation, norm) for i in range(n_hidden_layer)],
+        layers.Dense(n_output, b_gain=bias_coef)
     )
 
-    
+
 if __name__ == '__main__':
     import jax.numpy as np
     from jax import random
@@ -37,19 +46,23 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import os
 
-    net_init, net_fn = mlp(n_output=1, n_hidden_layer=2, n_hidden_unit=40, activation='relu', norm=None)
+    net_init, net_fn = mlp(n_output=1, n_hidden_layer=2, bias_coef=1.0, n_hidden_unit=40, activation='relu',
+                           norm='batch_norm')
 
     rng = random.PRNGKey(42)
     in_shape = (-1, 1)
     out_shape, net_params = net_init(rng, in_shape)
+
 
     def loss(params, batch):
         inputs, targets = batch
         predictions = net_fn(params, inputs)
         return np.mean((predictions - targets) ** 2)
 
+
     opt_init, opt_update, get_params = optimizers.momentum(step_size=1e-2, mass=0.9)
     opt_update = jit(opt_update)
+
 
     @jit
     def step(i, opt_state, batch):
@@ -57,23 +70,26 @@ if __name__ == '__main__':
         g = grad(loss)(params, batch)
         return opt_update(i, g, opt_state)
 
+
     task = sinusoid_task(n_support=1000, n_query=100)
 
     opt_state = opt_init(net_params)
     for i, (x, y) in enumerate(minibatch(task['x_train'], task['y_train'], batch_size=256, train_epochs=1000)):
         opt_state = step(i, opt_state, batch=(x, y))
-        if i == 0 or (i+1) % 100 == 0:
-            print(f"train loss: {loss(get_params(opt_state), (task['x_train'], task['y_train']))},\ttest loss: {loss(get_params(opt_state), (task['x_test'], task['y_test']))}")
-
-
-    output_dir = os.path.expanduser('~/code/neural-tangents/output')
-
-    x_true = np.linspace(-5, 5, 1000).reshape(-1, 1)
-    y_true = task['amp'] * np.sin(x_true - task['phase'])
-    plt.plot(x_true, y_true, 'k-', linewidth=0.5, label='true')
-
-    y_pred = net_fn(get_params(opt_state), x_true)
-    plt.plot(x_true, y_pred, 'b-', linewidth=0.5, label='network')
-
-    plt.legend()
-    plt.savefig(fname=os.path.join(output_dir, 'sinusoid_single_task_regression.png'))
+        if i == 0 or (i + 1) % 100 == 0:
+            print(
+                f"train loss: {loss(get_params(opt_state), (task['x_train'], task['y_train']))},"
+                f"\ttest loss: {loss(get_params(opt_state), (task['x_test'], task['y_test']))}"
+            )
+    #
+    # output_dir = os.path.expanduser('~/code/neural-tangents/output')
+    #
+    # x_true = np.linspace(-5, 5, 1000).reshape(-1, 1)
+    # y_true = task['amp'] * np.sin(x_true - task['phase'])
+    # plt.plot(x_true, y_true, 'k-', linewidth=0.5, label='true')
+    #
+    # y_pred = net_fn(get_params(opt_state), x_true)
+    # plt.plot(x_true, y_pred, 'b-', linewidth=0.5, label='network')
+    #
+    # plt.legend()
+    # plt.savefig(fname=os.path.join(output_dir, 'sinusoid_single_task_regression.png'))
--- maml/ntk_maml.py
+++ maml/ntk_maml.py
@@ -8,8 +8,6 @@ from functools import partial
 from network import mlp
 from data import sinusoid_task, taskbatch
 import os
-import matplotlib.pyplot as plt
-import plotly
 import numpy as onp
 import argparse
 import ipdb
@@ -23,8 +21,9 @@ from neural_tangents import tangents
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='sinusoid',
                     help='sinusoid or omniglot or miniimagenet')
-parser.add_argument('--n_hidden_layer', type=int, default=2)
-parser.add_argument('--n_hidden_unit', type=int, default=256)
+parser.add_argument('--n_hidden_layer', type=int, default=8)
+parser.add_argument('--n_hidden_unit', type=int, default=1024)
+parser.add_argument('--bias_coef', type=float, default=1.0)
 parser.add_argument('--activation', type=str, default='relu')
 parser.add_argument('--norm', type=str, default=None)
 parser.add_argument('--outer_step_size', type=float, default=1e-3)
@@ -32,19 +31,21 @@ parser.add_argument('--outer_opt_alg', type=str, default='adam',
                     help='adam or sgd or momentum')
 parser.add_argument('--inner_opt_alg', type=str, default='sgd',
                     help='sgd or momentum or adam')
-parser.add_argument('--inner_step_size', type=float, default=5e-2)
-parser.add_argument('--n_inner_step', type=int, default=2)
+parser.add_argument('--inner_step_size', type=float, default=(5e-1)/3)
+parser.add_argument('--n_inner_step', type=int, default=3*3)
 parser.add_argument('--task_batch_size', type=int, default=1)
-parser.add_argument('--n_train_task', type=int, default=20000)
-parser.add_argument('--n_support', type=int, default=20)
+parser.add_argument('--n_train_task', type=int, default=10000)
+parser.add_argument('--n_support', type=int, default=50)
 parser.add_argument('--n_query', type=int, default=50)
 parser.add_argument('--output_dir', type=str, default=os.path.expanduser('~/code/neural-tangents/output'))
-parser.add_argument('--exp_name', type=str, default='exp003')
-# parser.add_argument('--run_name', type=str, default=datetime.datetime.now().strftime('%d-%m-%Y_%H:%M:%S:%f'))
-parser.add_argument('--run_name', type=str, default='debug')
+parser.add_argument('--exp_name', type=str, default='exp004')
+parser.add_argument('--run_name', type=str, default=datetime.datetime.now().strftime('%d-%m-%Y_%H:%M:%S:%f'))
+parser.add_argument('--debug', action='store_true')
 
 # derive additional args and serialize
 args = parser.parse_args()
+if args.debug:
+    args.run_name = 'debug'
 assert args.task_batch_size == 1
 args.log_dir = os.path.join(args.output_dir, args.exp_name, args.run_name)
 os.makedirs(args.log_dir, exist_ok=True)
@@ -55,15 +56,19 @@ viz = Visdom(port=8000, env=f'{args.exp_name}_{args.run_name}')
 viz.text(json.dumps(obj=vars(args), sort_keys=True, indent=4))
 
 # build network
-net_init, f = mlp(n_output=1, n_hidden_layer=args.n_hidden_layer, n_hidden_unit=args.n_hidden_unit,
-                  activation=args.activation, norm=args.norm)
+net_init, f = mlp(n_output=1,
+                  n_hidden_layer=args.n_hidden_layer,
+                  n_hidden_unit=args.n_hidden_unit,
+                  bias_coef=args.bias_coef,
+                  activation=args.activation,
+                  norm=args.norm)
 
 # loss functions
 loss = lambda fx, y_hat: 0.5 * np.mean((fx - y_hat) ** 2)
 grad_loss = jit(grad(lambda p, x, y: loss(f(p, x), y)))
 param_loss = jit(lambda p, x, y: loss(f(p, x), y))
 
-# optimizers
+# optimizers    #TODO: separate optimizers for nonlinear and linear?
 outer_opt_init, outer_opt_update, outer_get_params = select_opt(args.outer_opt_alg, args.outer_step_size)()
 inner_opt_init, inner_opt_update, inner_get_params = select_opt(args.inner_opt_alg, args.inner_step_size)()
 
@@ -72,6 +77,65 @@ task_eval = sinusoid_task(n_support=args.n_support, n_query=args.n_query)
 
 
 def inner_optimization(params_init, x_train, y_train, n_inner_step):
+    state = inner_opt_init(params_init)
+    for i in range(n_inner_step):
+        p = inner_get_params(state)
+        g = grad_loss(p, x_train, y_train)
+        state = inner_opt_update(i, g, state)
+    p = inner_get_params(state)
+    loss_train = param_loss(p, x_train, y_train)
+    return p, loss_train
+
+
+@jit
+def maml_loss(params_init, task):
+    p, loss_train_eval = inner_optimization(
+        params_init, task_eval['x_train'], task_eval['y_train'], args.n_inner_step
+    )
+    loss_test_eval = param_loss(p, task_eval['x_test'], task_eval['y_test'])
+    fx_train_eval_adapt = f(p, task_eval['x_train'])
+    fx_test_eval_adapt = f(p, task_eval['x_test'])
+    fx_train_eval_init = f(params_init, task_eval['x_train'])
+    fx_test_eval_init = f(params_init, task_eval['x_test'])
+
+    # current task for MAML update
+    x_train, y_train, x_test, y_test = task
+    p, loss_train, = inner_optimization(
+        params_init, x_train, y_train, args.n_inner_step
+    )
+    loss_test = param_loss(p, x_test, y_test)
+    fx_train_adapt = f(p, x_train)
+    fx_test_adapt = f(p, x_test)
+    fx_train_init = f(params_init, x_train)
+    fx_test_init = f(params_init, x_test)
+
+    aux = {}
+    for v in [
+        'loss_train_eval',
+        'loss_test_eval',
+        'loss_train',
+        'loss_test',
+        'fx_train_eval_adapt',
+        'fx_test_eval_adapt',
+        'fx_train_eval_init',
+        'fx_test_eval_init',
+        'fx_train_adapt',
+        'fx_test_adapt',
+        'fx_train_init',
+        'fx_test_init',
+    ]:
+        aux[v] = locals()[v]
+    return loss_test, aux
+
+
+@jit
+def outer_step(i, state, task):
+    params = outer_get_params(state)
+    g, aux = grad(maml_loss, has_aux=True)(params, task)
+    return outer_opt_update(i, g, state), aux
+
+
+def inner_optimization_lin(params_init, x_train, y_train, n_inner_step):
     f_inner = tangents.linearize(f, params_init)
     grad_loss_inner = jit(grad(lambda p, x, y: loss(f_inner(p, x), y)))
     param_loss_inner = jit(lambda p, x, y: loss(f_inner(p, x), y))
@@ -83,125 +147,220 @@ def inner_optimization(params_init, x_train, y_train, n_inner_step):
         state = inner_opt_update(i, g, state)
     p = inner_get_params(state)
     l_train = param_loss_inner(p, x_train, y_train)
-    return p, l_train, param_loss_inner
+    return p, l_train, f_inner, param_loss_inner
 
 
 @jit
-def maml_loss(params_init, task):
-    p, l_train_eval, param_loss_inner = inner_optimization(params_init, task_eval['x_train'], task_eval['y_train'],
-                                                            args.n_inner_step)
-    l_test_eval = param_loss_inner(p, task_eval['x_test'], task_eval['y_test'])
+def maml_loss_lin(params_init, task):
+    # evaluation task
+    p, loss_train_eval_lin, f_inner, param_loss_inner = inner_optimization_lin(
+        params_init, task_eval['x_train'], task_eval['y_train'], args.n_inner_step
+    )
+    loss_test_eval_lin = param_loss_inner(p, task_eval['x_test'], task_eval['y_test'])
+    fx_train_eval_adapt_lin = f_inner(p, task_eval['x_train'])
+    fx_test_eval_adapt_lin = f_inner(p, task_eval['x_test'])
+    fx_train_eval_init_lin = f_inner(params_init, task_eval['x_train'])
+    fx_test_eval_init_lin = f_inner(params_init, task_eval['x_test'])
 
     # current task for MAML update
     x_train, y_train, x_test, y_test = task
-    p, l_train, param_loss_inner = inner_optimization(params_init, x_train, y_train, args.n_inner_step)
-    l_test = param_loss_inner(p, x_test, y_test)
-    aux = dict(loss_train=l_train, loss_test=l_test, loss_train_eval=l_train_eval, loss_test_eval=l_test_eval)
-    return l_test, aux
+    p, loss_train_lin, f_inner, param_loss_inner, = inner_optimization_lin(
+        params_init, x_train, y_train, args.n_inner_step
+    )
+    loss_test_lin = param_loss_inner(p, x_test, y_test)
+    fx_train_adapt_lin = f_inner(p, x_train)
+    fx_test_adapt_lin = f_inner(p, x_test)
+    fx_train_init_lin = f_inner(params_init, x_train)
+    fx_test_init_lin = f_inner(params_init, x_test)
+
+    aux = {}
+    for v in [
+        'loss_train_eval_lin',
+        'loss_test_eval_lin',
+        'loss_train_lin',
+        'loss_test_lin',
+        'fx_train_eval_adapt_lin',
+        'fx_test_eval_adapt_lin',
+        'fx_train_eval_init_lin',
+        'fx_test_eval_init_lin',
+        'fx_train_adapt_lin',
+        'fx_test_adapt_lin',
+        'fx_train_init_lin',
+        'fx_test_init_lin',
+    ]:
+        aux[v] = locals()[v]
+    return loss_test_lin, aux
 
 
 @jit
-def outer_step(i, state, task):
+def outer_step_lin(i, state, task):
     params = outer_get_params(state)
     # g, aux = grad(maml_loss, has_aux=True)(params, task)
-    g, aux = grad(maml_loss, has_aux=True)(params, task)
+    g, aux = grad(maml_loss_lin, has_aux=True)(params, task)
     return outer_opt_update(i, g, state), aux
 
 
 # logging, plotting
-aux_keys = ['loss_train', 'loss_test']
-aux_eval_keys = ['loss_train_eval', 'loss_test_eval']
-log = Log(keys=['update'] + aux_keys + aux_eval_keys)
-win_loss, win_loss_eval = None, None
+win_loss_keys = ['loss_train', 'loss_test', 'loss_train_lin', 'loss_test_lin']
+win_loss_eval_keys = ['loss_train_eval', 'loss_test_eval', 'loss_train_eval_lin', 'loss_test_eval_lin']
+win_rmse_keys = ['rmse_train_adapt', 'rmse_test_adapt', 'rmse_train_init', 'rmse_test_init']
+win_rmse_eval_keys = ['rmse_train_eval_adapt', 'rmse_test_eval_adapt', 'rmse_train_eval_init', 'rmse_test_eval_init']
+all_plot_keys = win_loss_keys + win_loss_eval_keys + win_rmse_keys + win_rmse_eval_keys
+log = Log(keys=['update'] + all_plot_keys)
 
 _, params = net_init(rng=random.PRNGKey(42), input_shape=(-1, 1))
-state = outer_opt_init(params)
+outer_state = outer_opt_init(params)
+outer_state_lin = outer_opt_init(params)
+
+rmse = jit(lambda fx, fx_lin: np.sqrt(np.mean(fx - fx_lin) ** 2))
+
+
+class VisdomPlotter:
+    def __init__(self, viz):
+        self.viz = viz
+        self.windows = {}
+
+    def line(self, win_name, log, plot_keys, title, xlabel, ylabel, X):
+        Y = onp.stack([log[key] for key in plot_keys] +
+                      [onp.convolve(log[key], [0.05] * 20, 'same') for key in plot_keys], axis=1)
+        if win_name not in self.windows:
+            self.windows[win_name] = self.viz.line(
+                X=X, Y=Y,
+                opts=dict(title=title, xlabel=xlabel, ylabel=ylabel,
+                          legend=plot_keys + [f'{key}_smooth' for key in plot_keys],
+                          dash=onp.array(['dot' for i in range(len(plot_keys) * 2)])
+                          )
+            )
+        else:
+            self.viz.line(
+                X=X, Y=Y, win=self.windows[win_name], update='replace'
+            )
+
+
+plotter = VisdomPlotter(viz)
+
 for i, task_batch in tqdm(enumerate(taskbatch(task_fn=sinusoid_task,
                                               batch_size=args.task_batch_size,
                                               n_task=args.n_train_task,
                                               n_support=args.n_support,
                                               n_query=args.n_query))):
-    state, aux = outer_step(i=i, state=state, task=(task_batch['x_train'],
-                                                    task_batch['y_train'],
-                                                    task_batch['x_test'],
-                                                    task_batch['y_test']))
-    log.append([(key, aux[key]) for key in aux_keys + aux_eval_keys] + [('update', i)])
+    outer_state, aux_nonlin = outer_step(
+        i=i,
+        state=outer_state,
+        task=(task_batch['x_train'],
+              task_batch['y_train'],
+              task_batch['x_test'],
+              task_batch['y_test']))
+    outer_state_lin, aux_lin = outer_step_lin(
+        i=i,
+        state=outer_state_lin,
+        task=(task_batch['x_train'],
+              task_batch['y_train'],
+              task_batch['x_test'],
+              task_batch['y_test']))
 
-    if (i + 1) % (args.n_train_task // args.task_batch_size // 100) == 0:
-        win_loss_X = log['update']
-        win_loss_Y = onp.stack([log[key] for key in aux_keys] +
-                               [onp.convolve(log[key], [0.05] * 20, 'same') for key in aux_keys], axis=1)
-        if win_loss is None:
-            win_loss = viz.line(
-                X=win_loss_X,
-                Y=win_loss_Y,
-                opts=dict(title=f'maml sinusoid regression on meta-training tasks',
-                          xlabel='update',
-                          ylabel='half l2 loss',
-                          legend=['train', 'test', 'train_smooth', 'test_smooth'],
-                          dash=onp.array(['dot' for i in range(len(aux_keys) * 2)]))
-            )
-        else:
-            viz.line(
-                X=win_loss_X,
-                Y=win_loss_Y,
-                win=win_loss,
-                update='replace'
-            )
+    aux = {**aux_nonlin, **aux_lin}
+    assert (len(aux.keys()) == len(aux_nonlin.keys()) + len(aux_lin.keys()))
 
-        win_loss_eval_X = log['update']
-        win_loss_eval_Y = onp.stack([log[key] for key in aux_eval_keys] +
-                                    [onp.convolve(log[key], [0.05] * 20, 'same') for key in aux_eval_keys], axis=1)
-        if win_loss_eval is None:
-            win_loss_eval = viz.line(
-                X=win_loss_eval_X,
-                Y=win_loss_eval_Y,
-                opts=dict(title=f'maml sinusoid regression on evaluation task',
-                          xlabel='update',
-                          ylabel='half l2 loss',
-                          legend=['train', 'test', 'train_smooth', 'test_smooth'],
-                          dash=onp.array(['dot' for i in range(len(aux_eval_keys) * 2)]))
-            )
-        else:
-            viz.line(
-                X=win_loss_eval_X,
-                Y=win_loss_eval_Y,
-                win=win_loss_eval,
-                update='replace'
-            )
+    for k in ['train_adapt', 'test_adapt', 'train_init', 'test_init',
+              'train_eval_adapt', 'test_eval_adapt', 'train_eval_init', 'test_eval_init']:
+        aux[f'rmse_{k}'] = rmse(aux[f'fx_{k}'], aux[f'fx_{k}_lin'])
 
-# visualize model
-params_maml = outer_get_params(state)
+    log.append([('update', i)])
+    log.append([(key, aux[key]) for key in all_plot_keys])
+
+    if (i + 1) % (args.n_train_task // args.task_batch_size // 20) == 0:
+        plotter.line(
+            win_name='loss',
+            log=log,
+            plot_keys=win_loss_keys,
+            title='maml sinusoid regression on meta-training tasks',
+            xlabel='maml update',
+            ylabel='post-adaptation half l2 loss',
+            X=log['update']
+        )
+        plotter.line(
+            win_name='loss_eval',
+            log=log,
+            plot_keys=win_loss_eval_keys,
+            title='maml sinusoid regression on fixed evaluation task',
+            xlabel='maml update',
+            ylabel='post-adaptation half l2 loss',
+            X=log['update']
+        )
+        plotter.line(
+            win_name='rmse',
+            log=log,
+            plot_keys=win_rmse_keys,
+            title='rmse between nonlinear and linear model outputs on meta-training tasks',
+            xlabel='maml update',
+            ylabel='rmse',
+            X=log['update']
+        )
+        plotter.line(
+            win_name='rmse_eval',
+            log=log,
+            plot_keys=win_rmse_eval_keys,
+            title='rmse between nonlinear and linear model outputs on fixed evaluation task',
+            xlabel='maml update',
+            ylabel='rmse',
+            X=log['update']
+        )
+
+# visualize models
+
+params = outer_get_params(outer_state)
+params_lin = outer_get_params(outer_state_lin)
 xrange_inputs = np.linspace(-5, 5, 100).reshape(-1, 1)
 targets = np.sin(xrange_inputs)
 win_inference = viz.line(
     Y=targets,
     X=xrange_inputs,
     name='target',
-    opts=dict(title='sinusoid inference',
+    opts=dict(title='nonlinear model sinusoid inference',
               xlabel='x',
               ylabel='y',
               )
 )
 
-f_lin = tangents.linearize(f, params_maml)
+win_inference_lin = viz.line(
+    Y=targets,
+    X=xrange_inputs,
+    name='target',
+    opts=dict(title='linear model sinusoid inference',
+              xlabel='x',
+              ylabel='y',
+              )
+)
+
+f_lin = tangents.linearize(f, params_lin)
 grad_loss_lin = jit(grad(lambda p, x, y: loss(f_lin(p, x), y)))
 param_loss_lin = jit(lambda p, x, y: loss(f_lin(p, x), y))
 
-predictions = f_lin(params_maml, xrange_inputs)
+predictions = f(params, xrange_inputs)
 viz.line(Y=predictions, X=xrange_inputs, win=win_inference, update='append', name='pre-update predictions')
+predictions_lin = f_lin(params_lin, xrange_inputs)
+viz.line(Y=predictions_lin, X=xrange_inputs, win=win_inference_lin, update='append', name='pre-update predictions')
 
 x1 = onp.random.uniform(low=-5., high=5., size=(args.n_support, 1))
 y1 = 1. * onp.sin(x1 + 0.)
 
-state = inner_opt_init(params_maml)
+state = inner_opt_init(params)
+state_lin = inner_opt_init(params_lin)
 for i in range(1, args.n_inner_step + 1):
     p = inner_get_params(state)
-    g = grad_loss_lin(p, x1, y1)
+    g = grad_loss(p, x1, y1)
     state = inner_opt_update(i, g, state)
     p = inner_get_params(state)
-    predictions = f_lin(p, xrange_inputs)
+    predictions = f(p, xrange_inputs)
     viz.line(Y=predictions, X=xrange_inputs, win=win_inference, update='append', name=f'{i}-step predictions')
+
+    p_lin = inner_get_params(state_lin)
+    g_lin = grad_loss_lin(p_lin, x1, y1)
+    state_lin = inner_opt_update(i, g_lin, state_lin)
+    p_lin = inner_get_params(state_lin)
+    predictions_lin = f_lin(p_lin, xrange_inputs)
+    viz.line(Y=predictions_lin, X=xrange_inputs, win=win_inference_lin, update='append', name=f'{i}-step predictions')
 
 # serialize
 np_dir = os.path.join(args.log_dir, 'np')
